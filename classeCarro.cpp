00a123505590cd25c5803132bf053db2e214c44f
thecookiebutter <127798899+thecookiebutter@users.noreply.github.com>
2023-04-24 19:59:49 -0300

    Update classeCarro.cpp

--- classeCarro.cpp
+++ classeCarro.cpp
@@ -1,11 +1,11 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-
+//Criacao da classe Carro
 class Carro
 {
     public:
-    
+    //Variaveis e metodo da classe Carro
     string Cor;
     string Modelo;
     float VelMax;
@@ -13,17 +13,19 @@ class Carro
 };
 
 int main() {
+    //Instancia da classe Carro
     Carro MeuCarro;
-
+    //Variaveis da instancia
     MeuCarro.Cor = "Branco";
     MeuCarro.Modelo = "Fusca";
     MeuCarro.VelMax = 150.0f;
-    
+    //Imprimindo metodo e variaveis da instancia
     MeuCarro.Buzinar();
     cout << MeuCarro.Cor << endl;
     cout << MeuCarro.Modelo << endl;
     cout << MeuCarro.VelMax << endl;
 }
+    //Metodo da classe Carro 
      void Carro::Buzinar(){
     cout << "Bi,Bi!!!" << endl;
     };
